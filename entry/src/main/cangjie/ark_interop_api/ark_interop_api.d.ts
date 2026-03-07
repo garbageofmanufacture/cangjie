643c27ac27eb10cb4e353a21d12f31ea401d699f
Zhang Runtao <zhangruntao001@example.com>
2026-03-07 21:41:23 +0800

    你的提交信息

---
+++ entry/src/main/cangjie/ark_interop_api/ark_interop_api.d.ts
@@ -0,0 +1,6 @@
+export declare interface CustomLib {
+    registerJSFunc(name: string, fn: () => void): void
+    unregisterJSFunc(name: string): void
+    registerJSFunc(name: string, fn: () => void): void
+    unregisterJSFunc(name: string): void
+}
---
+++ entry/src/main/cangjie/types/libohos_app_cangjie_entry/Index.d.ts
@@ -0,0 +1,14 @@
+export declare function testCJ(s: string): string
+
+export function registerJSFunc(arg0: string, arg1: () => void) {
+  throw new Error('Function not implemented.');
+}
+
+
+export function unregisterJSFunc(arg0: string) {
+  throw new Error('Function not implemented.');
+}
+
+export declare function registerJSFunc(name: string, fn: () => void): void
+
+export declare function unregisterJSFunc(name: string): void
